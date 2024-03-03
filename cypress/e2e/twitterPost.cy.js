045d1bc320f7a68d48236a6d0d044282ed8a7105
michael-garnett <garnett.michael79@gmail.com>
2024-03-03 04:55:35 -0500

    xpath plugin

--- cypress/e2e/twitterPost.cy.js
+++ cypress/e2e/twitterPost.cy.js
@@ -1,9 +1,19 @@
 describe('Twitter marketing post', () => {
+
+  
+
   it('passes', () => {
     cy.visit('https://twitter.com/')
     cy.contains('Sign in').click()
-    cy.get('input[name="text"]').type(usernameOrNumb)
+    cy.get('input[name="text"]').type(emailOrNumb)
     cy.contains('Next').click()
-    cy.get('input[name="password"]').type(Password)
+    if (cy.contains('There was unusual login activity on your account. To help keep your account safe, please enter your phone number or username to verify it’s you.')) {
+      cy.get('input[name="text"]').type(numb)
+      cy.contains('Next').click()
+    } else {
+      //do nothing
+    }
+    cy.get('input[name="password"]').type(password)
+    cy.contains('Log in').click()
   })
 })
