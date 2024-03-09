6553713e632be3f90d66e33a1ff821fd0b79a559
michael-garnett <garnett.michael79@gmail.com>
2024-03-09 18:00:17 -0500

    screenshots and video

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter', //for html reports
+  screenshots: "on-failure", // Capture screenshots on failure
+  video: true, // Enable video recording for all tests (including passing ones)
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      screenshotOnRunFailure=true;
+      require('cypress-mochawesome-reporter/plugin')(on); // for html eports
+
     },
   },
 });
