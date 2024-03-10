6b535e650d8b076c766e0e6f20eb455ad594bb7a
michael-garnett <garnett.michael79@gmail.com>
2024-03-09 19:20:19 -0500

    POM 2.0

--- cypress/PageObjects/TwitterPOM.js
+++ cypress/PageObjects/TwitterPOM.js
@@ -1,56 +1,66 @@
 class TwitterPOM{
 
+    txtUsername='input[name="text"]'
+    txtPassword='input[name="password"]'
+    txtNumber='input[name="text"]'
+    btnSignIn='Sign in'
+    btnNext='Next'
+    btnLogin='Log in'
+    btnAccounts='header[role="banner"] div:nth-child(4) div:nth-child(1)'
+    btnLogOut='body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)'
+    txtTweetBody='div[data-block="true"] div'
+
     setUserName(username)
     {
-        cy.get('input[name="text"]').type(username)
+        cy.get(this.txtUsername).type(username)
     }
 
     setPassword(password)
     {
-        cy.get('input[name="password"]').type(password)
+        cy.get(this.txtPassword).type(password)
     }
 
     setNumber(number)
     {
-        cy.get('input[name="text"]').type(number)
+        cy.get(this.txtNumber).type(number)
     }
 
     clickSignIn()
     {
-        cy.contains('Sign in').click()
+        cy.contains(this.btnSignIn).click()
     }
 
     clickNext()
     {
-        cy.contains('Next').click()
+        cy.contains(this.btnNext).click()
     }
 
     clickLogin()
     {
-        cy.contains('Log in').click()
+        cy.contains(this.btnLogin).click()
     }
 
     clickAccounts()
     {
-        cy.get('header[role="banner"] div:nth-child(4) div:nth-child(1)')
+        cy.get(this.btnAccounts)
         .click()
     }
 
     clickLogOut()
     {
-        cy.get('body > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(2) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1)')
+        cy.get(this.btnLogOut)
         .click() 
     }
 
     verifyLogin()
     {
         cy.wait(2500)
-        cy.contains('Log in').should('not.be.visible')
+        cy.contains(this.btnLogin).should('not.be.visible')
     }
 
     TweetBody(body)
     {
-        cy.get('div[data-block="true"] div').type(body)
+        cy.get(txtTweetBody).type(body)
     }
 
 }
